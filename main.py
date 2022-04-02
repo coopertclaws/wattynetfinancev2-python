3794fc0501da72bc48a73cb9a1472f3e214d690a
Ben Watson <benwatson@Bens-Mac-mini.local>
2022-04-02 16:26:12 +0100

    first deployment of scheduled task

--- main.py
+++ main.py
@@ -1,74 +1,85 @@
 import os
 import mysql.connector
 import logging
+from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
 
+msg = "Running database script..."
+print(msg)
+
+today = datetime.now()
+if today.day == 2:
+    print("Run script today!")
+else:
+    print("Not today...")
+
+
 # Connect to database
+def monthly_script():
+    mydb = mysql.connector.connect (
+        host=os.getenv('DB_HOST'),
+        user=os.getenv('DB_USER'),
+        password=os.getenv('DB_PASSWORD'),
+        database=os.getenv('DATABASE')
+    )
+    # Create db connection buffers
+    curA = mydb.cursor(buffered=True)
+    curB = mydb.cursor(buffered=True)
+    curC = mydb.cursor(buffered=True)
+    curD = mydb.cursor(buffered=True)
 
-mydb = mysql.connector.connect (
-    host=os.getenv('DB_HOST'),
-    user=os.getenv('DB_USER'),
-    password=os.getenv('DB_PASSWORD'),
-    database=os.getenv('DATABASE')
-)
-# Create db connection buffers
-curA = mydb.cursor(buffered=True)
-curB = mydb.cursor(buffered=True)
-curC = mydb.cursor(buffered=True)
-curD = mydb.cursor(buffered=True)
+    # Define SQL statements
+    get_virtual_accounts = (
+        "SELECT id AS vaccount, starting_balance, current_balance, amount FROM virtual_account"
+    )
 
-# Define SQL statements
-get_virtual_accounts = (
-    "SELECT id AS vaccount, starting_balance, current_balance, amount FROM virtual_account"
-)
+    sum_virtual_account = (
+        "SELECT SUM(amount) AS Total FROM transactions WHERE virtual_account = %s"
+    )
 
-sum_virtual_account = (
-    "SELECT SUM(amount) AS Total FROM transactions WHERE virtual_account = %s"
-)
+    update_balance = (
+        "UPDATE virtual_account SET starting_balance = %s WHERE id = %s"
+    )
 
-update_balance = (
-    "UPDATE virtual_account SET starting_balance = %s WHERE id = %s"
-)
+    copy_transactions = (
+        "INSERT INTO transactionshistory (user, virtual_account, amount, timestamp, tofrom, description) SELECT user, virtual_account, amount, timestamp, tofrom, description FROM transactions"
+    )
 
-copy_transactions = (
-    "INSERT INTO transactionshistory (user, virtual_account, amount, timestamp, tofrom, description) SELECT user, virtual_account, amount, timestamp, tofrom, description FROM transactions"
-)
+    drop_transactions = (
+        "TRUNCATE TABLE transactions"
+    )
 
-drop_transactions = (
-    "TRUNCATE TABLE transactions"
-)
+    # List all virtual accounts
+    curA.execute(get_virtual_accounts)
 
-# List all virtual accounts
-curA.execute(get_virtual_accounts)
+    # Store as an array
+    vaccount_array = curA.fetchall()
 
-# Store as an array
-vaccount_array = curA.fetchall()
+    # Loop through each item of the array
+    for (vaccount, starting_balance, current_balance, amount) in vaccount_array:
+        curB.execute(sum_virtual_account, (vaccount,))
+        totals_array = curB.fetchall()
+        for (Total) in totals_array:
+            Total = (Total[0])
+            try:
+                new_balance = (Total + starting_balance + amount)
+            except TypeError:
+                new_balance = (starting_balance + amount)
+            if (new_balance - amount) != current_balance:
+                logging.warning("Balance error for virtual account")
+            curC.execute(update_balance, (new_balance, vaccount))
+            mydb.commit()
+            logging.info(curC.rowcount, "record(s) affected")
 
-# Loop through each item of the array
-for (vaccount, starting_balance, current_balance, amount) in vaccount_array:
-    curB.execute(sum_virtual_account, (vaccount,))
-    totals_array = curB.fetchall()
-    for (Total) in totals_array:
-        Total = (Total[0])
-        try:
-            new_balance = (Total + starting_balance + amount)
-        except TypeError:
-            new_balance = (starting_balance + amount)
-        if (new_balance - amount) != current_balance:
-            logging.warning("Balance error for virtual account")
-        curC.execute(update_balance, (new_balance, vaccount))
+    copysuccess = False
+    try:
+        curD.execute(copy_transactions)
         mydb.commit()
-        logging.info(curC.rowcount, "record(s) affected")
-
-copysuccess = False
-try:
-    curD.execute(copy_transactions)
-    mydb.commit()
-    copysuccess = True
-except mysql.connector.Error as e:
-    logging.warning("Error:", e)
-if copysuccess:
-    curD.execute(drop_transactions)
-    mydb.commit()
-    logging.info("success!!!")
+        copysuccess = True
+    except mysql.connector.Error as e:
+        logging.warning("Error:", e)
+    if copysuccess:
+        curD.execute(drop_transactions)
+        mydb.commit()
+        logging.info("success!!!")
